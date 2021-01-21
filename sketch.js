a9d989e4ae5408169ac9c3c21f197f6f9c53fd54
poptarts2006 <76689222+poptarts2006@users.noreply.github.com>
2021-01-21 17:45:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var movingRect, fixedRect ;
+
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(400, 200, 80, 30);
+  movingRect.shapeColor = 'green';
+  fixedRect = createSprite(200, 200, 50, 80);
+  fixedRect.shapeColor = 'green';
+}
+
+function draw() {
+  background(0); 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+   if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect .width/2 && 
+    fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect .width/2 && 
+    movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect .height/2 && 
+    fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2 ){
+     movingRect.shapeColor = 'red';
+     fixedRect.shapeColor = 'blue';
+   }
+   else {
+    movingRect.shapeColor = 'green';
+    fixedRect.shapeColor = 'green';
+   }
+  drawSprites();
+}
